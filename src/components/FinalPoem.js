b7dfdbf5e1565e1e685d50a57b541fd5fc3f7f93
Jessica Liang <jessicursliang@gmail.com>
2020-04-20 19:06:21 -0700

    format display of lines on separate lines

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './FinalPoem.css';
 
 const FinalPoem = (props) => {
-  console.log(props);
+  const formattedLines = props.submittedLines.map((line) => {
+    return(
+      <p key={ line }>
+        { line }
+      </p>
+    );
+  });
+
   return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
-          { props.submittedLines }
+          { formattedLines }
       </section>
 
       <div className="FinalPoem__reveal-btn-container">
