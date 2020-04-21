5211d12cd457893b3b28ed5a21544a4ab261dbf0
Corinna Fabre <corinna.fabre@gmail.com>
2020-04-20 19:08:43 -0700

    Built out PlayerSubmissionForm and skeleton Recent Submission

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import './RecentSubmission.css';
+import PlayerSubmissionForm from './PlayerSubmissionForm';
 
 const RecentSubmission = (props) => {
+  const compiledSubmissions = props.submissions.map((submission) => {
+    return(
+      <PlayerSubmissionForm 
+      adjOne={submission.adjOne} 
+      nounOne={submission.nounOne} 
+      adverb={submission.adverb} 
+      verb={submission.verb} 
+      adjTwo={submission.adjTwo} 
+      nounTwo={submission.nounTwo} />
+    )
+  });
+
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
