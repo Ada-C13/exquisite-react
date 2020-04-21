86a61507f3b48365dcf53eca94771e9da18f9b49
Vera <lizeth.veraro@outlook.com>
2020-04-20 19:05:46 -0700

    onChange function, and state for new poem, HTML form

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -2,23 +2,83 @@ import React, { useState } from 'react';
 import './PlayerSubmissionForm.css';
 
 const PlayerSubmissionForm = () => {
+
+  const [poemPiece, setPoemPiece] = useState({
+    adj1:'',
+    noun1:'',
+    adv:'',
+    verb:'',
+    adj2:'',
+    noun2:''
+  });
+
+  const onChange =(event) => {
+    
+    const newPoem = {
+      ...poemPiece
+    }
+    console.log(newPoem);
+    newPoem[event.target.name] = event.target.value;
+    setPoemPiece(newPoem);
+  }
+
+  
+
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{  }</h3>
 
       <form className="PlayerSubmissionForm__form" >
-
         <div className="PlayerSubmissionForm__poem-inputs">
-
-          {
-            // Put your form inputs here... We've put in one below as an example
-          }
-          <input
-            placeholder="hm..."
-            type="text" />
-
+          The
+        <input 
+          name="adj1"
+          placeholder="adjective" 
+          type="text" 
+          className="PlayerSubmissionForm__input--invalid" 
+          value={poemPiece.adj1}
+          onChange={onChange}
+        />  
+        <input 
+          name="noun1"
+          placeholder="noun" 
+          type="text" 
+          className="PlayerSubmissionForm__input--invalid" 
+          value={poemPiece.noun1}
+          onChange={onChange}
+        />
+        <input 
+          name="adv"
+          placeholder="adverb" 
+          type="text" className="PlayerSubmissionForm__input--invalid" 
+          value={poemPiece.adv}
+          onChange={onChange}
+        />
+        <input 
+          name="verb"
+          placeholder="verb" 
+          type="text" 
+          className="PlayerSubmissionForm__input--invalid" 
+          value={poemPiece.verb}
+          onChange={onChange}
+        />the
+        <input 
+          name="adj2"
+          placeholder="adjective" 
+          type="text" 
+          className="PlayerSubmissionForm__input--invalid" 
+          value={poemPiece.adj2}
+          onChange={onChange}
+        />
+        <input 
+          name="noun2"
+          placeholder="noun" 
+          type="text" 
+          className="PlayerSubmissionForm__input--invalid" 
+          value={poemPiece.noun2}
+          onChange={onChange}
+        />.
         </div>
-
         <div className="PlayerSubmissionForm__submit">
           <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
         </div>
