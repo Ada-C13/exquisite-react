c3bd10c4d94ba3c6e95310a9eb7601a9fc5a295d
SuelyBarreto <suely@barreto.us>
2020-04-20 21:12:23 -0700

    formToPoem

--- src/components/Game.js
+++ src/components/Game.js
@@ -18,17 +18,16 @@ const Game = () => {
     }
   }).join(" ");
 
-  const formToLine = (form) => {
+  const formToPoem = (form) => {
     return (`The ${form.adj1} ${form.noun1} ${form.adv} ${form.verb} the ${form.adj2} ${form.noun2}`);
   }
 
   const onFormSubmit = (formFields) => {
     console.log(`Form submitted`, formFields, poem, player)
     const updatedPoem = Array.from(poem);
-    updatedPoem.push(formToLine(formFields));
+    updatedPoem.push(formToPoem(formFields));
     setPoem(updatedPoem);
     setPlayer(player + 1);
-    console.log(`Form submitted`, formFields, poem, player)
   }
 
 
@@ -37,7 +36,7 @@ const Game = () => {
       <h2>Game</h2>
 
       <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
-      <p>{poem.length>0 ? poem[0]:`empty...` }</p>
+      <p>Debugging... {poem.length>0 ? poem[0]:`empty...` }</p> 
       <p>Please follow the following format for your poetry submission:</p>
 
       <p className="Game__format-example">
@@ -46,7 +45,7 @@ const Game = () => {
 
       <RecentSubmission />
 
-      <PlayerSubmissionForm field={FIELDS} player={player} isFinal={isFinal} onFormSubmit={onFormSubmit} />
+      <PlayerSubmissionForm player={player} isFinal={isFinal} onFormSubmit={onFormSubmit} />
 
       <FinalPoem />
 
