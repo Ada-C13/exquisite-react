493ce8eeb3528227705a9694337d54aed7773a71
Vera <lizeth.veraro@outlook.com>
2020-04-21 15:50:20 -0700

    PlayerSubmissionForm take props to use the callback to send back the poem data

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './PlayerSubmissionForm.css';
 
-const PlayerSubmissionForm = () => {
+const PlayerSubmissionForm = (props) => {
 
   const [poemPiece, setPoemPiece] = useState({
     adj1:'',
@@ -17,18 +17,43 @@ const PlayerSubmissionForm = () => {
     const newPoem = {
       ...poemPiece
     }
+    
     console.log(newPoem);
     newPoem[event.target.name] = event.target.value;
     setPoemPiece(newPoem);
   }
 
-  
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("submitting form")
+    if ( poemPiece.adj1 !== '' && 
+        poemPiece.noun1 !== '' && 
+        poemPiece.adv !== '' && 
+        poemPiece.verb !== '' && 
+        poemPiece.adj2 !== '' &&
+        poemPiece.noun2 !== '' )
+      {
+       // send that data back up to app. 
+       props.onFormSubmitCallback(poemPiece)
+       // Clear the form afert to submit the data back to the game.
+       setPoemPiece({
+        adj1 :'', 
+        noun1 :'', 
+        adv :'', 
+        verb :'', 
+        adj2 :'',
+        noun2 :'',
+       });
+      }
+  };
 
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{  }</h3>
 
-      <form className="PlayerSubmissionForm__form" >
+      <form 
+        onSubmit={onFormSubmit}
+        className="PlayerSubmissionForm__form" > 
         <div className="PlayerSubmissionForm__poem-inputs">
           The
         <input 
