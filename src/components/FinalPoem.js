e68ee17f6cd1422fcb2a2e92b3629dbda0f76523
Nataliya Pogodina <n.pogodina@gmail.com>
2020-04-21 09:10:00 -0700

    Added conditional rendering to FinalPoem: poem and button

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -4,13 +4,14 @@ import './FinalPoem.css';
 const FinalPoem = (props) => {
 
   const poemLines = props.poem.map((line) => {
-      return <div>{line}</div>
-  })
+    return <p>{line}</p>
+  });
 
   const onButtonClick = () => {
     props.onClickCallback();
   }
 
+  let poem = null;
   let button = (
     <div className="FinalPoem__reveal-btn-container">
       <input 
@@ -24,20 +25,18 @@ const FinalPoem = (props) => {
 
   if (props.inProgress === "no") {
     button = null;
+    poem = (
+      <section className="FinalPoem__poem">
+        <h3>Final Poem</h3>
+        {poemLines}
+      </section>
+    );
   }
 
   return (
     <div className="FinalPoem">
-      <section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
-
-      </section>
-
+      {poem}
       {button}
-
-      <div>
-        {poemLines}
-      </div>
     </div>
   )
 };
