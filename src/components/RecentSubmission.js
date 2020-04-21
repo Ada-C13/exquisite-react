556051fffd8977994043a3f3a9a8fd3faf6a9b87
Catherina <catherine.pchelina@gmail.com>
2020-04-21 15:50:09 -0700

    Add RecentSubmission component implementation

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './RecentSubmission.css';
 
 const RecentSubmission = (props) => {
+
+  const displayRecentSubmission = () =>  {
+    if (props.recentlySubmitted !== undefined) {
+      return (<p>{Object.values(props.recentlySubmitted).join(" ")}</p>);
+    }
+  }
+
+
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
+      {displayRecentSubmission()}
       <p className="RecentSubmission__submission">{  }</p>
     </div>
   );
