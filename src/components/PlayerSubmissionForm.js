e89aab2852b06476607f4cfffc84fa19332f00a1
Rosalyn <syrosalynyu@gmail.com>
2020-04-21 10:41:16 -0700

    structured the inputs and set the state in PlayerSubmissionForm

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import './PlayerSubmissionForm.css';
 
 const PlayerSubmissionForm = () => {
+  const [sentence, setSentene] = useState({
+    
+  });
+
+
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{  }</h3>
@@ -9,14 +14,34 @@ const PlayerSubmissionForm = () => {
       <form className="PlayerSubmissionForm__form" >
 
         <div className="PlayerSubmissionForm__poem-inputs">
-
           {
             // Put your form inputs here... We've put in one below as an example
           }
+          
           <input
-            placeholder="hm..."
+            placeholder="adjective"
             type="text" />
 
+          <input
+            placeholder="none"
+            type="text" />  
+
+          <input
+            placeholder="adverb"
+            type="text" />
+
+          <input
+            placeholder="verb"
+            type="text" />
+          the
+          <input
+            placeholder="adjective..."
+            type="text" />
+
+          <input
+            placeholder="none"
+            type="text" />
+          .
         </div>
 
         <div className="PlayerSubmissionForm__submit">
