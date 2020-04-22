2207d47763f3dcc40c95a3afe1903b5744aa6928
Olga Patrakova <olgapatrakova@outlook.com>
2020-04-21 19:48:04 -0700

    The page updates and doesn't display the fields when final poem is requested

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './FinalPoem.css';
+import PropTypes from 'prop-types';
 
 const FinalPoem = (props) => {
 
+  const onButtonClick = () => {
+    console.log("props.poem", props.poem)
+    displayPoem(props.poem)
+    // onFinalClick();
+    props.onFinalPoemClick();
+  };
+
+  let finalPoem;
+  const displayPoem = (poem) => {
+    // for (let line of poem) {
+    //   finalPoem += line;
+    // };
+    finalPoem = poem;
+  };
+
+  if (props.condition) {
   return (
+  <div className="FinalPoem">
+      <section className="FinalPoem__poem">
+      <h3>Final Poem</h3>
+      <p>{finalPoem}</p>
+      </section>
+    <div className="FinalPoem__reveal-btn-container" >
+      <input 
+        type="button" 
+        value="We are finished: Reveal the Poem" 
+        className="FinalPoem__reveal-btn" 
+        onClick={onButtonClick}
+      />
+    </div>
+  </div>
+  );
+  } else {
+    return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
-
+      <h3>Final Poem</h3>
+      <p>{finalPoem}</p>
       </section>
-
-      <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
-      </div>
     </div>
-  );
+    );
+  };
+};
+
+FinalPoem.propTypes = {
+  poem: PropTypes.array,
+  onFinalPoemClick: PropTypes.func.isRequired,
 }
 
 export default FinalPoem;
--- src/components/Game.js
+++ src/components/Game.js
@@ -14,6 +14,7 @@ const Game = () => {
   }).join(" ");
 
   const [poem, setPoemArray] = useState([]);
+  console.log('Poem', poem);
 
   const addLine = (line) => {
     let newPoemLine = '';
@@ -24,38 +25,62 @@ const Game = () => {
     const newPoem = poem;
     newPoem.push(newPoemLine);
     newPoemLine = '';
+    console.log('newPoem', newPoem);
     setPoemArray(newPoem);
     addPlayer()
   };
+
+  // State for player count
   const [count, setCount] = useState(1);
-  
+
   const addPlayer = () => {
     setCount(count + 1)
   };
 
+  const [condition, setCondition] = useState(true);
+  const onFinalPoemClick = () => {
+    setCondition(false);
+  };
 
+  if (condition) {
+    return (
+      <div className="Game">
+        <h2>Game</h2>
 
-  return (
-    <div className="Game">
-      <h2>Game</h2>
+        <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
 
-      <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
+        <p>Please follow the following format for your poetry submission:</p>
 
-      <p>Please follow the following format for your poetry submission:</p>
+        <p className="Game__format-example">
+          {exampleFormat}
+        </p>
 
-      <p className="Game__format-example">
-        { exampleFormat }
-      </p>
+        <RecentSubmission />
 
-      <RecentSubmission />
+        <PlayerSubmissionForm fields={FIELDS} onSubmitCallback={addLine} count={count} />
 
-      <PlayerSubmissionForm fields={FIELDS} onSubmitCallback={addLine} count={count}/>
+        <FinalPoem poem={poem} onFinalPoemClick={onFinalPoemClick} condition={condition}/>
 
-      <FinalPoem />
+      </div>
+    );
+  } else {
+    return (
+      <div className="Game">
+        <h2>Game</h2>
 
-    </div>
-  );
-}
+        <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
+
+        <p>Please follow the following format for your poetry submission:</p>
+
+        <p className="Game__format-example">
+          {exampleFormat}
+        </p>
+        <FinalPoem poem={poem} onFinalPoemClick={onFinalPoemClick} condition={condition}/>
+
+      </div>
+    );
+  };
+};
 
 
 const FIELDS = [
