c1052ced86b9a1987e3090492a112bacc55c6691
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-04-21 23:39:37 -0700

    player can type, click, delete and it has placeholder for each field.

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import './FinalPoem.css';
+import React from "react";
+import "./FinalPoem.css";
 
-const FinalPoem = (props) => {
+const FinalPoem = ({ isGameOver, onFinalClick }) => {
+  //when the user clicks on button I need to return the final poem
 
   return (
     <div className="FinalPoem">
-      <section className="FinalPoem__poem">
-        <h3>Final Poem</h3>
-
-      </section>
-
-      <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
-      </div>
+      {isGameOver ? (
+        <section className="FinalPoem__poem">
+          <h3>Final Poem</h3>
+        </section>
+      ) : (
+        <div className="FinalPoem__reveal-btn-container">
+          <input
+            onClick={onFinalClick} //
+            type="button"
+            value="We are finished: Reveal the Poem"
+            className="FinalPoem__reveal-btn"
+          />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default FinalPoem;
