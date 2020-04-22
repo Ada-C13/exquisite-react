4ed617b136e1d0ea6a151a0861389b5e2959ec9f
Vera <lizeth.veraro@outlook.com>
2020-04-21 23:51:32 -0700

    CLEARINPUTS constant to store the input empty string and some comments

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import './PlayerSubmissionForm.css';
+
+const CLEARINPUTS = {
+  adj1:'',
+  noun1:'',
+  adv:'',
+  verb:'',
+  adj2:'',
+  noun2:''
+};
 
 const PlayerSubmissionForm = (props) => {
 
-  const [poemPiece, setPoemPiece] = useState({
-    adj1:'',
-    noun1:'',
-    adv:'',
-    verb:'',
-    adj2:'',
-    noun2:''
-  });
+  // State to handle the pieces of poems.
+  // CLEARINPUTS = use constant to set the input to empty strings.
+  const [poemPiece, setPoemPiece] = useState(CLEARINPUTS);
 
   const onChange =(event) => {
-    
     const newPoem = {
       ...poemPiece
     }
@@ -26,25 +29,18 @@ const PlayerSubmissionForm = (props) => {
   const onFormSubmit = (event) => {
     event.preventDefault();
     console.log("submitting form")
-    if ( poemPiece.adj1 !== '' && 
+    if (poemPiece.adj1 !== '' && 
         poemPiece.noun1 !== '' && 
         poemPiece.adv !== '' && 
         poemPiece.verb !== '' && 
         poemPiece.adj2 !== '' &&
         poemPiece.noun2 !== '' )
-      {
-       // send that data back up to app. 
-       props.onFormSubmitCallback(poemPiece)
-       // Clear the form afert to submit the data back to the game.
-       setPoemPiece({
-        adj1 :'',
-        noun1 :'', 
-        adv :'', 
-        verb :'', 
-        adj2 :'',
-        noun2 :'',
-       });
-      }
+    {
+      // send that data back up to app. 
+      props.onFormSubmitCallback(poemPiece)
+      // reseting form with no text in the input elements after the first player submitted
+      setPoemPiece(CLEARINPUTS);
+    }
   };
 
   // Getting the fields from the parent (Game) Props.field object.
@@ -56,6 +52,7 @@ const PlayerSubmissionForm = (props) => {
           <input 
             key={i} 
             name={field.key} 
+            // able to see placeholder text in each text input. 
             placeholder= {field.placeholder} 
             value={valueHolder}
             onChange={onChange}
@@ -67,14 +64,19 @@ const PlayerSubmissionForm = (props) => {
       }
   });
 
+  console.log(props)
+
   return (
     <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{  }</h3>
+      {/* Displaying the number of the current player. props.currentPlayer from Game.js */}
+      <h3>Player Submission Form for Player #{props.currentPlayer}</h3>
 
       <form 
+        // Invoking onFormSubmit function. When the player submit the piece of their poem. 
         onSubmit={onFormSubmit}
         className="PlayerSubmissionForm__form" > 
         <div className="PlayerSubmissionForm__poem-inputs">
+          {/* Invoking the function to generate dynamicly the Input form. */}
          {generateFormComponents}
         </div>
         <div className="PlayerSubmissionForm__submit">
