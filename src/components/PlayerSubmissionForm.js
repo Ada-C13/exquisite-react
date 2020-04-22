5630d7fbd7d892862a0cd046f15fa08633d84e87
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-04-22 15:47:51 -0700

    form fields will not submit unless all fields have text

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,17 +1,66 @@
 import React, { useState } from 'react';
 import './PlayerSubmissionForm.css';
+// import PropTypes from 'prop-types';
 
-const PlayerSubmissionForm = () => {
+const PlayerSubmissionForm = ({ onSubmitCallBack }) => {
 
-  // formFields Hook goes here
+  // formFields hook goes here
+  const [formFields, setFormFields ] = useState({
+    the1: "The",
+    adj1: '',
+    noun1: '',
+    adv: '',
+    verb: '',
+    the2: 'the',
+    adj2: '',
+    noun2: ''
+
+  });
+
   // onInputChange goes here
+  const onInputChange = (event) => {
+    const newFormFields = {
+      ...formFields,
+    };
+
+    newFormFields[event.target.name] = event.target.value;
+    setFormFields(newFormFields);
+  };
+
   // textValidation goes here
+  // not sure what to do about textValidation
+
   // onFormSubmit goes here
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const formFieldsArray = Object.values(formFields);
+
+    if(!formFieldsArray.includes('')) {
+      console.log('Success! Form Submitted!');
+
+      onSubmitCallBack(formFields);
+
+      setFormFields({
+        the1: "The",
+        adj1: '',
+        noun1: '',
+        adv: '',
+        verb: '',
+        the2: 'the',
+        adj2: '',
+        noun2: ''
+
+      });
+    }
+  }
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{  }</h3>
 
-      <form className="PlayerSubmissionForm__form" >
+      <form 
+        className="PlayerSubmissionForm__form"
+        onSubmit={ onFormSubmit }>
 
         <div className="PlayerSubmissionForm__poem-inputs">
         The
@@ -19,43 +68,44 @@ const PlayerSubmissionForm = () => {
           name="adj1"
           placeholder="adjective"
           type="text"
-          // noChange={onInputChange}
-          // value={formFields.adj1}
+          // DRY
+          onChange={onInputChange}
+          value={formFields.adj1}
         />
         <input
           name="noun1"
           placeholder="noun"
           type="text"
-          // noChange={onInputChange}
-          // value={formFields.noun1}
+          onChange={onInputChange}
+          value={formFields.noun1}
         />
         <input
           name="adv"
           placeholder="adverb"
           type="text"
-          // noChange={onInputChange}
-          // value={formFields.adv}
+          onChange={onInputChange}
+          value={formFields.adv}
         />
         <input
           name="verb"
           placeholder="verb"
           type="text"
-          // noChange={onInputChange}
-          // value={formFields.verb}
+          onChange={onInputChange}
+          value={formFields.verb}
         />
         <input
           name="adj2"
           placeholder="adjective"
           type="text"
-          // noChange={onInputChange}
-          // value={formFields.adj2}
+          onChange={onInputChange}
+          value={formFields.adj2}
         />
         <input
           name="noun2"
           placeholder="noun"
           type="text"
-          // noChange={onInputChange}
-          // value={formFields.noun2}
+          onChange={onInputChange}
+          value={formFields.noun2}
         />
         </div>
 
@@ -68,5 +118,11 @@ const PlayerSubmissionForm = () => {
 }
 
 // PropTypes go here
+// PlayerSubmissionForm.propTypes = {
+  // onFormSubmit: PropTypes.func.isRequired,
+  // onSubmitCallBack: PropTypes.func.isRequired,
+  // formFieldsArray: PropTypes.array.isRequired
+
+// };
 
 export default PlayerSubmissionForm;
