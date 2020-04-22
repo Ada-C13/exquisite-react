38f6c00a39918e665d5248c57c3102e64169df9d
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-04-22 13:14:06 -0700

    create 6 inputs and write comments for next steps

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -2,6 +2,11 @@ import React, { useState } from 'react';
 import './PlayerSubmissionForm.css';
 
 const PlayerSubmissionForm = () => {
+
+  // formFields Hook goes here
+  // onInputChange goes here
+  // textValidation goes here
+  // onFormSubmit goes here
   return (
     <div className="PlayerSubmissionForm">
       <h3>Player Submission Form for Player #{  }</h3>
@@ -9,14 +14,49 @@ const PlayerSubmissionForm = () => {
       <form className="PlayerSubmissionForm__form" >
 
         <div className="PlayerSubmissionForm__poem-inputs">
-
-          {
-            // Put your form inputs here... We've put in one below as an example
-          }
-          <input
-            placeholder="hm..."
-            type="text" />
-
+        The
+        <input
+          name="adj1"
+          placeholder="adjective"
+          type="text"
+          noChange={onInputChange}
+          value={formFields.adj1}
+        />
+        <input
+          name="noun1"
+          placeholder="noun"
+          type="text"
+          noChange={onInputChange}
+          value={formFields.noun1}
+        />
+        <input
+          name="adv"
+          placeholder="adverb"
+          type="text"
+          noChange={onInputChange}
+          value={formFields.adv}
+        />
+        <input
+          name="verb"
+          placeholder="verb"
+          type="text"
+          noChange={onInputChange}
+          value={formFields.verb}
+        />
+        <input
+          name="adj2"
+          placeholder="adjective"
+          type="text"
+          noChange={onInputChange}
+          value={formFields.adj2}
+        />
+        <input
+          name="noun2"
+          placeholder="noun"
+          type="text"
+          noChange={onInputChange}
+          value={formFields.noun2}
+        />
         </div>
 
         <div className="PlayerSubmissionForm__submit">
@@ -27,5 +67,6 @@ const PlayerSubmissionForm = () => {
   );
 }
 
+// PropTypes go here
 
 export default PlayerSubmissionForm;
