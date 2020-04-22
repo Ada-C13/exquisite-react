53bdfab10aaab32311c7768c778b4ca83c913c64
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-04-21 23:00:03 -0700

    Sub Form- submit and Change event

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,31 +1,91 @@
-import React, { useState } from 'react';
-import './PlayerSubmissionForm.css';
+import React, { useState } from "react";
+import "./PlayerSubmissionForm.css";
 
 const PlayerSubmissionForm = () => {
+  const initialState = {
+    adjective: "",
+    noun: "",
+    adverb: "",
+    verb: "",
+    adjectiveTwo: "",
+    nounTwo: "",
+  };
+  const [formFields, setFormFields] = useState(initialState);
+
+  const onInputChange = (event) => {
+    console.log(`Changing field ${event.target.name} to ${event.target.value}`);
+    const newFormField = {
+      ...formFields,
+    };
+    newFormField[event.target.name] = event.target.value;
+    setFormFields(newFormField);
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    setFormFields({
+      ...initialState,
+    });
+  };
+
   return (
     <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{  }</h3>
+      <h3>Player Submission Form for Player #{}</h3>
 
-      <form className="PlayerSubmissionForm__form" >
-
+      <form className="PlayerSubmissionForm__form">
         <div className="PlayerSubmissionForm__poem-inputs">
-
           {
             // Put your form inputs here... We've put in one below as an example
           }
+          The
           <input
-            placeholder="hm..."
-            type="text" />
-
+            name="adjective"
+            placeholder="adjective"
+            onChange={onInputChange}
+            value={formFields.adjective}
+          />
+          <input
+            name="noun"
+            onChange={onInputChange}
+            placeholder="noun"
+            value={formFields.noun}
+          />
+          <input
+            onChange={onInputChange}
+            placeholder="adverb"
+            value={formFields.adverb}
+          />
+          <input
+            name="adverb"
+            onChange={onInputChange}
+            placeholder="verb"
+            value={formFields.verb}
+          />
+          the
+          <input
+            name="adjectiveTwo"
+            onChange={onInputChange}
+            placeholder="adjective"
+            value={formFields.adjectiveTwo}
+          />
+          <input
+            name="nounTwo"
+            onChange={onInputChange}
+            placeholder="noun"
+            value={formFields.nounTwo}
+          />
         </div>
-
         <div className="PlayerSubmissionForm__submit">
-          <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+          <input
+            onSubmit={onFormSubmit}
+            type="submit"
+            value="Submit Line"
+            className="PlayerSubmissionForm__submit-btn"
+          />
         </div>
       </form>
     </div>
   );
-}
-
+};
 
 export default PlayerSubmissionForm;
