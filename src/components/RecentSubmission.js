1a070839a000e1d96a000be0a76f231bda5cde17
Vera <lizeth.veraro@outlook.com>
2020-04-22 13:32:54 -0700

    showingLastPoemePiece func to show the last piece of the poem

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './RecentSubmission.css';
 
 const RecentSubmission = (props) => {
+
+  const showingLastPoemePiece =() => {
+    // 
+    const lastPoemIdx = props.poems.length - 1
+    return (
+      <p key={lastPoemIdx}>{props.poems[lastPoemIdx]}</p>
+    )
+  };
+
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{  }</p>
+      { showingLastPoemePiece() }
+      <p className="RecentSubmission__submission">{ }</p>
     </div>
   );
 }
