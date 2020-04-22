8cecf9660eb99bd1e4cc025682d77925b4d26503
Sara Nilsen <sarabrandaoalves@gmail.com>
2020-04-21 23:37:18 -0700

    game has a list of all submissions

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import './Game.css';
-import PlayerSubmissionForm from './PlayerSubmissionForm';
-import FinalPoem from './FinalPoem';
-import RecentSubmission from './RecentSubmission';
-
+import React, { useState } from "react";
+import "./Game.css";
+import PlayerSubmissionForm from "./PlayerSubmissionForm";
+import FinalPoem from "./FinalPoem";
+import RecentSubmission from "./RecentSubmission";
+//Game component to have the data of my submission, so that the Game component keeps track of all of the submissions.
 const Game = () => {
   const exampleFormat = FIELDS.map((field) => {
     if (field.key) {
@@ -13,55 +13,81 @@ const Game = () => {
     }
   }).join(" ");
 
+  const [listSubmissions, setListSubmissions] = useState([]);
+  // const [isGameOver, setGameOver] = useState(false);
+
+  const onSubmitClick = (submission) => {
+    //give me that as string so I can push to the array?
+    const { adjective, noun, adverb, verb, adjectiveTwo, nounTwo } = submission;
+    const newSubmissions = [...listSubmissions];
+    const sentence = `The ${adjective} ${noun} ${adverb} ${verb} the ${adjectiveTwo} ${nounTwo}`;
+    newSubmissions.push(sentence);
+    setListSubmissions(newSubmissions);
+    console.log(listSubmissions);
+  };
+
+  // const onFinalClick = () => {
+  //   setGameOver(true);
+  // };
+
   return (
     <div className="Game">
       <h2>Game</h2>
 
-      <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
+      <p>
+        Each player should take turns filling out and submitting the form below.
+        Each turn should be done individually and <em>in secret!</em> Take
+        inspiration from the revealed recent submission. When all players are
+        finished, click the final button on the bottom to reveal the entire
+        poem.
+      </p>
 
       <p>Please follow the following format for your poetry submission:</p>
 
-      <p className="Game__format-example">
-        { exampleFormat }
-      </p>
+      <p className="Game__format-example">{exampleFormat}</p>
 
-      <RecentSubmission />
+      <RecentSubmission mostRecentSubmission={listSubmissions.slice(-1)} />
 
-      <PlayerSubmissionForm />
+      {/* <PlayerSubmissionForm
+        playerNumber={listSubmissions.length + 1}
+        onSubmitClick={onSubmitClick}
+      /> */}
 
-      <FinalPoem />
-
+      {/* <FinalPoem
+        finalList={listSubmissions}
+        onFinalClick={onFinalClick}
+        isGameOver={isGameOver}
+      /> */}
     </div>
   );
-}
-
+};
 
 const FIELDS = [
   "The",
   {
-    key: 'adj1',
-    placeholder: 'adjective',
+    key: "adj1",
+    placeholder: "adjective",
   },
   {
-    key: 'noun1',
-    placeholder: 'noun',
+    key: "noun1",
+    placeholder: "noun",
   },
   {
-    key: 'adv',
-    placeholder: 'adverb',
+    key: "adv",
+    placeholder: "adverb",
   },
   {
-    key: 'verb',
-    placeholder: 'verb',
+    key: "verb",
+    placeholder: "verb",
   },
   "the",
   {
-    key: 'adj2',
-    placeholder: 'adjective',
+    key: "adj2",
+    placeholder: "adjective",
   },
   {
-    key: 'noun2',
-    placeholder: 'noun',
+    key: "noun2",
+    placeholder: "noun",
   },
   ".",
 ];
