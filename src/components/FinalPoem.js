9e991ac8361feec1c6e41a34d69d267cac836f21
Vera <lizeth.veraro@outlook.com>
2020-04-22 12:40:54 -0700

    FinalPoem func to display each part of the poem inside of a <p>

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -3,13 +3,23 @@ import './FinalPoem.css';
 
 const FinalPoem = (props) => {
 
+  console.log(props)
+
+  // Each submission in the final poem section on a different line or paragraph.
+  const showingPoemLines = props.poems.map((poem, i) => {
+    return (
+      <p key={i}>{poem}</p>
+    )
+  });
+
   return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
-
+    
       </section>
-
+        {/* See all of the submissions of poetry lines in the section named "Final Poem". */}
+        {showingPoemLines}
       <div className="FinalPoem__reveal-btn-container">
         <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" />
       </div>
