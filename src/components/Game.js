67e23b267716dde24a394cf86e733f83ada4cfc7
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-04-22 20:14:07 -0700

    finished wave 2 and wave 3.

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,7 +4,7 @@ import PlayerSubmissionForm from './PlayerSubmissionForm';
 import FinalPoem from './FinalPoem';
 import RecentSubmission from './RecentSubmission';
 
-const Game = () => {
+const Game = (props) => {
   const exampleFormat = FIELDS.map((field) => {
     if (field.key) {
       return field.placeholder;
@@ -12,29 +12,41 @@ const Game = () => {
       return field;
     }
   }).join(" ");
+  const [mostRecentSubmissionText, setMostRecentSubmissionText] = useState("")
+  const [submissions, setSubmissions] = useState([])
+  const [showComponent, setShowComponent] = useState(false)
+  const createPoem = (poem) =>{
+    const newSubmissions = [...submissions]
+      newSubmissions.push({
+        adjective1:poem.adjective1,
+        noun1: poem.noun1,
+        adverb: poem.adverb,
+        verb: poem.verb,
+        adjective2: poem.adjective2,
+        noun2:poem.noun2 
+      });
+    setSubmissions(newSubmissions);
+    setMostRecentSubmissionText("The Most Recent Submission")
+  }
 
+  const displayFinalPoem = () =>{
+    setShowComponent(!showComponent);
+  }
+  
   return (
     <div className="Game">
       <h2>Game</h2>
-
       <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
-
       <p>Please follow the following format for your poetry submission:</p>
-
       <p className="Game__format-example">
         { exampleFormat }
       </p>
-
-      <RecentSubmission />
-
-      <PlayerSubmissionForm />
-
-      <FinalPoem />
-
+      {!showComponent && <RecentSubmission submissions = {submissions} mostRecentSubmissionText={mostRecentSubmissionText}/>}
+      {!showComponent && <PlayerSubmissionForm onSubmitCallback = {createPoem}/>}
+      {<FinalPoem submissions = {submissions} onClickCallback = {displayFinalPoem} showComponent = {showComponent}/>}
     </div>
-  );
+  ); 
 }
-
 
 const FIELDS = [
   "The",
--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './RecentSubmission.css';
-
+import PropTypes from 'prop-types';
 const RecentSubmission = (props) => {
+  const submissions = props.submissions.map(input =>{
+    return(
+      `The ${input.adjective1} 
+      ${input.noun1} 
+      ${input.adverb} 
+      ${input.verb} the
+      ${input.adjective2} 
+      ${input.noun2}`
+    )
+  })
+  
   return (
     <div className="RecentSubmission">
-      <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{  }</p>
-    </div>
+      <h3>{props.mostRecentSubmissionText}</h3>
+      <p className="RecentSubmission__submission">{submissions[submissions.length - 1]}</p>
+    </div> 
   );
+}
+
+RecentSubmission.propTypes = {
+  submissions: PropTypes.arrayOf(PropTypes.object),
+  mostRecentSubmissionText: PropTypes.string
 }
 
 export default RecentSubmission;
