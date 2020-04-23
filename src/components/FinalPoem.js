55fdd56e82ef9e6c0e386159cc5a2de50947d2d9
Haben <haben7@gmail.com>
2020-04-22 19:37:54 -0700

    formatted finalPoem

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./FinalPoem.css";
 
-
 const FinalPoem = (props) => {
-  const formatPoem =props.submissions.map(submission => 
-    <p>The {submission.adj1} {submission.noun1} {submission.adv} {submission.verb} the {submission.adj2} {submission.noun2}.</p>
-    ) // this should return an array of strings not array of objects
+  const formatPoem = props.submissions.map((submission) => (
+    <p>
+      The {submission.adj1} {submission.noun1} {submission.adv}{" "}
+      {submission.verb} the {submission.adj2} {submission.noun2}.
+    </p>
+  )); // this should return an array of strings not array of objects
 
-
+  // show final poem if the final poem button has been clicked
   const showFinalPoem = () => {
     if (props.revealBox) {
       return (
@@ -21,29 +23,16 @@ const FinalPoem = (props) => {
         </div>
       );
     } else {
-      return (
-        <div>
-         {formatPoem}
-        </div>
-      )
+      return <div>{formatPoem}</div>;
     }
   };
 
-  console.log(props.submissions);
   return (
     <div className="FinalPoem">
       <section className="FinalPoem__poem">
         <h3>Final Poem</h3>
       </section>
       {showFinalPoem()}
-      {/* <div className="FinalPoem__reveal-btn-container">
-        <input
-          type="button"
-          value="We are finished: Reveal the Poem"
-          className="FinalPoem__reveal-btn"
-          onClick={props.onFinalPoem}
-        />
-      </div> */}
     </div>
   );
 };
