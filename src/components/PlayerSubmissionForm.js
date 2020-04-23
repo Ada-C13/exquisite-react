16fde2572a2c7c00f041979f6b8a925dfd180d68
Alex Robertson <arobertson335@gmail.com>
2020-04-23 09:23:45 -0700

    Submitting what I have done so far.

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -1,31 +1,101 @@
 import React, { useState } from 'react';
 import './PlayerSubmissionForm.css';
+import PropTypes from 'prop-types'; 
 
-const PlayerSubmissionForm = () => {
+const PlayerSubmissionForm = (props) => {
+   
+  const [form, setFormTemplate] = useState(
+    {
+      adjOne: "",
+      nounOne: "",
+      advOne: "",
+      verbOne: "",
+      adjTwo: "",
+      nounTwo: ""
+    }
+  );
+
+  const [poet, setPoet] = useState(1)
+
+  const poetCount = () => {
+    setPoet (poet++);
+  };
+
+  const onClickSubmit = (event) => {
+    event.preventDefault();
+    console.log("Your poem has been submitted.")
+    //...
+  };
+
+  //Things this file needs to do:
+  //Send a poem submission into a data collection in Game.js 
+  //Reset the state of the form fields so they're back to placeholder values after onClickSubmit. 
+  //
+
+
+
+
+
+
+
+
+
+  const render = () => {
   return (
     <div className="PlayerSubmissionForm">
-      <h3>Player Submission Form for Player #{  }</h3>
-
+      <h3>Player Submission Form for Player #{/*poetCount()*/} :</h3>
       <form className="PlayerSubmissionForm__form" >
-
         <div className="PlayerSubmissionForm__poem-inputs">
+            <input 
+            name="adjOne"
+            placeholder="Adjective"
+            type="text"
+            />
 
-          {
-            // Put your form inputs here... We've put in one below as an example
-          }
-          <input
-            placeholder="hm..."
-            type="text" />
+            <input
+              name="nounOne"
+              placeholder="Noun"
+              type="text"
+            />
 
+            <input
+              name="advOne"
+              placeholder="Adverb"
+              type="text"
+            />
+
+            <input
+              name="verbOne"
+              placeholder="Verb"
+              type="text"
+            />
+
+            <input
+              name="adjTwo"
+              placeholder="Adjective"
+              type="text"
+            />
+
+            <input
+              name="nounTwo"
+              placeholder="Noun"
+              type="text"
+            />
         </div>
 
         <div className="PlayerSubmissionForm__submit">
-          <input type="submit" value="Submit Line" className="PlayerSubmissionForm__submit-btn" />
+          <input 
+          type="submit" 
+          value="Submit Line" 
+          className="PlayerSubmissionForm__submit-btn" />
         </div>
+
       </form>
     </div>
   );
+  }
 }
+
 
 
 export default PlayerSubmissionForm;
--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -4,7 +4,7 @@ import './RecentSubmission.css';
 const RecentSubmission = (props) => {
   return (
     <div className="RecentSubmission">
-      <h3>The Most Recent Submission</h3>
+      <h3>The Most Recent Submission:</h3>
       <p className="RecentSubmission__submission">{  }</p>
     </div>
   );
