ef40e9966b23315f245c0d81411f1ae2d3ce472b
Haben <haben7@gmail.com>
2020-04-22 19:39:27 -0700

    added condition to not show after submission

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import './RecentSubmission.css';
+import React from "react";
+import "./RecentSubmission.css";
 
 const RecentSubmission = (props) => {
-  return (
-    <div className="RecentSubmission">
-      <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{ props.recentSubmission }</p>
-    </div>
-  );
-}
+  const showRecentSubmission = () => {
+    if (props.revealBox) {
+      return (
+        <div className="RecentSubmission">
+          <h3>{props.recentSubmission ? "The Most Recent Submission" : ""}</h3>
+          <p className="RecentSubmission__submission">
+            {props.recentSubmission}
+          </p>
+        </div>
+      );
+    }
+  };
+  return <div>{showRecentSubmission()}</div>;
+};
 
 export default RecentSubmission;
