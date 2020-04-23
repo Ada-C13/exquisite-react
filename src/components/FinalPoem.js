f6897bf18b4e040f5bbba914d33889fd067a92d0
Halahaddad1 <h.m.haddad@gmail.com>
2020-04-23 00:01:17 -0700

    cleaned up comments

--- src/components/FinalPoem.js
+++ src/components/FinalPoem.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
-import './FinalPoem.css';
+import React, { useState } from "react";
+import "./FinalPoem.css";
 
 const FinalPoem = (props) => {
-
-  
-  return (
-    <div className="FinalPoem" >
-      {props.gameon&& <section className="FinalPoem__poem" >
-        <h3>Final Poem</h3>
-        {props.poems.map ((poem)=> <p>{poem}</p>)}
-      </section  >}
-      <div className="FinalPoem__reveal-btn-container">
-        <input type="button" value="We are finished: Reveal the Poem" className="FinalPoem__reveal-btn" onClick={props.onPoemSubmit} />
-      </div>
-    </div>
-  );
-}
+	return (
+		<div className="FinalPoem">
+			{props.gameon && (
+				<section className="FinalPoem__poem">
+					<h3>Final Poem</h3>
+					{props.poems.map((poem) => (
+						<p>{poem}</p>
+					))}
+				</section>
+			)}
+			<div className="FinalPoem__reveal-btn-container">
+				<input
+					type="button"
+					value="We are finished: Reveal the Poem"
+					className="FinalPoem__reveal-btn"
+					onClick={props.onPoemSubmit}
+				/>
+			</div>
+		</div>
+	);
+};
 
 export default FinalPoem;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,123 +1,119 @@
-import React, { useState } from 'react';
-import './Game.css';
-import PlayerSubmissionForm from './PlayerSubmissionForm';
-import FinalPoem from './FinalPoem';
-import RecentSubmission from './RecentSubmission';
-
+import React, { useState } from "react";
+import "./Game.css";
+import PlayerSubmissionForm from "./PlayerSubmissionForm";
+import FinalPoem from "./FinalPoem";
+import RecentSubmission from "./RecentSubmission";
 
 const Game = () => {
-  const inputFormat = FIELDS.map((field) => {
-    if (field.key) {
-      return field.placeholder;
-    } else {
-      return field;
-    }
-  }).join(" ");
+	const inputFormat = FIELDS.map((field) => {
+		if (field.key) {
+			return field.placeholder;
+		} else {
+			return field;
+		}
+	}).join(" ");
 
+	const [player, setPlayer] = useState(1);
 
-  const [player, setPlayer] = useState(1);
+	const incrementPlayer = (player) => {
+		const newPlayer = player + 1;
+		setPlayer(newPlayer);
+	};
 
-  const incrementPlayer = (player) =>{
-    const newPlayer = player + 1;
-    setPlayer(newPlayer);
-  }
+	const [poemList, setPoemList] = useState([]);
+	const [gameon, setGameOn] = useState(false);
 
-  const [poemList, setPoemList] = useState([]);
-  const [gameon, setGameOn]= useState(false)
+	console.log(`this comes from game`, poemList[poemList.length - 1]);
 
-  // const endGame = () => {(gameon? setGameOn(true) : setGameOn(false))}
-  console.log(`this comes from game`, poemList[poemList.length-1]);
+	const onPoemSubmit = () => {
+		setGameOn(true);
+	};
 
+	const addPoem = (poem) => {
+		const newPoemList = [...poemList];
 
-  const onPoemSubmit = () => {
-    setGameOn(true)
-  };
+		newPoemList.push({
+			key: player,
+			the1: "The",
+			adj1: poem.adj1,
+			noun1: poem.noun1,
+			adv: poem.adv,
+			verb: poem.verb,
+			the2: "the",
+			adj2: poem.adj2,
+			noun2: poem.noun2,
+			dot: ".",
+		});
 
-  const addPoem = (poem) => {
+		setPoemList(newPoemList);
+		incrementPlayer(player);
+	};
 
-    const newPoemList = [...poemList];
+	const poems = poemList.map(
+		(poem) =>
+			`${poem.the1} ${poem.adj1} ${poem.noun1} ${poem.adv} ${poem.verb} ${poem.the2} ${poem.adj2} ${poem.noun2}${poem.dot}`
+	);
 
-    // Find the max id and add 1
-    // const nextId = newPoemList.reduce((accumulator, currentPoem) => {
-      // return Math.max(accumulator, currentPoem.id);
-    //  }, 0) + 1;
+	console.log(`this comes from game`, poemList[poemList.length - 1]);
 
-    newPoemList.push({
-      key: player,
-      the1: "The",
-      adj1: poem.adj1,
-      noun1: poem.noun1,
-      adv: poem.adv,
-      verb: poem.verb,
-      the2: "the",
-      adj2: poem.adj2,
-      noun2: poem.noun2,
-      dot: "."
-    });
+	return (
+		<div className="Game">
+			<h2>Game</h2>
 
-    setPoemList(newPoemList);
-    incrementPlayer(player);
-    
-    
-  }
-   
+			<p>
+				Each player should take turns filling out and submitting the form below.
+				Each turn should be done individually and <em>in secret!</em> Take
+				inspiration from the revealed recent submission. When all players are
+				finished, click the final button on the bottom to reveal the entire
+				poem.
+			</p>
 
-  const poems =  poemList.map((poem) => `${poem.the1} ${poem.adj1} ${poem.noun1} ${poem.adv} ${poem.verb} ${poem.the2} ${poem.adj2} ${poem.noun2}${poem.dot}`);
-  
-  console.log(`this comes from game`, poemList[poemList.length-1]);
+			<p>Please follow the following format for your poetry submission:</p>
 
+			<p className="Game__format-example">{inputFormat}</p>
 
-  return (
-    <div className="Game">
-      <h2>Game</h2>
+			<RecentSubmission gameon={gameon} poem={poemList[poemList.length - 1]} />
 
-      <p>Each player should take turns filling out and submitting the form below. Each turn should be done individually and <em>in secret!</em> Take inspiration from the revealed recent submission. When all players are finished, click the final button on the bottom to reveal the entire poem.</p>
+			<PlayerSubmissionForm
+				gameon={gameon}
+				addPoemCallback={addPoem}
+				player={player}
+				fields={FIELDS}
+			/>
 
-      <p>Please follow the following format for your poetry submission:</p>
-
-       <p className="Game__format-example">
-        { inputFormat }
-      </p>
-
-      <RecentSubmission gameon={gameon} poem={poemList[poemList.length-1]}/>
-
-      <PlayerSubmissionForm gameon={gameon} addPoemCallback={addPoem} player={player} fields={FIELDS}/>
-
-      <FinalPoem poems={poems} gameon={gameon} onPoemSubmit={onPoemSubmit}/>
-
-    </div>
-  );
-}
-
+			<FinalPoem poems={poems} gameon={gameon} onPoemSubmit={onPoemSubmit} />
+		</div>
+	);
+};
 
 const FIELDS = [
-  "The",
-  {
-    key: 'adj1',
-    placeholder: 'adjective',
-  },
-  {
-    key: 'noun1',
-    placeholder: 'noun',
-  },
-  {
-    key: 'adv',
-    placeholder: 'adverb',
-  },
-  {
-    key: 'verb',
-    placeholder: 'verb',
-  },
-  "the",
-  {
-    key: 'adj2',
-    placeholder: 'adjective',
-  },
-  {
-    key: 'noun2',
-    placeholder: 'noun',
-  },
-  ".",
+	"The",
+	{
+		key: "adj1",
+		placeholder: "adjective",
+	},
+	{
+		key: "noun1",
+		placeholder: "noun",
+	},
+	{
+		key: "adv",
+		placeholder: "adverb",
+	},
+	{
+		key: "verb",
+		placeholder: "verb",
+	},
+	"the",
+	{
+		key: "adj2",
+		placeholder: "adjective",
+	},
+	{
+		key: "noun2",
+		placeholder: "noun",
+	},
+	".",
 ];
 
 export default Game;
