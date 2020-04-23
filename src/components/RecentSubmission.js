8c66814258c8ce0cb4134a24e1ddf7fd98ace9a3
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-04-22 19:54:37 -0700

    add comments/clean file

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -5,13 +5,16 @@ const RecentSubmission = ({ poem }) => {
   let submission = []
   let mostRecentSubmission = poem.slice(-1)[0];
 
+
+// As a player, I only want to see the "The Most Recent Submission" section if 
+// there has already been at least one submission. If no submission, return empty string.
   if (mostRecentSubmission === undefined) {
     submission.push('')
   } else {
     submission.push(Object.values(mostRecentSubmission).join(" ").concat('.'));
   }
-
- return (
+  
+  return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
       <p className="RecentSubmission__submission">
