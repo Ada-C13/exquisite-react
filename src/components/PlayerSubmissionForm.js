7b5e0d5dd73127dbf9e5702390629d0bcbb532da
thenora <thenora@gmail.com>
2020-04-23 01:20:28 -0700

    set use states in playersubmissionform

--- src/components/PlayerSubmissionForm.js
+++ src/components/PlayerSubmissionForm.js
@@ -12,6 +12,18 @@ const PlayerSubmissionForm = () => {
       noun2: ""
   });
 
+  const onInputChange = event => {
+    const { name, value } = event.target;
+
+      const newFields = {
+        ...fields,
+        [name]: value // [name] get value of name and use it as the key | value is the variable above
+        // line 46 is equivalent to: newFields[name] = value
+      };
+  
+    setFields(newFields);
+  };
+
 
   return (
     <div className="PlayerSubmissionForm">
@@ -22,32 +34,50 @@ const PlayerSubmissionForm = () => {
         <div className="PlayerSubmissionForm__poem-inputs">
           The
           <input
+            name="adj1"
+            onChange={onInputChange}
+            value={fields.adj1}
             placeholder="adjective"
             type="text" 
           />
           
           <input
+            name="noun1"
+            onChange={onInputChange}
+            value={fields.noun1}
             placeholder="noun"
             type="text" 
           />
           
           <input
+            name="adverb"
+            onChange={onInputChange}
+            value={fields.adverb}
             placeholder="adverb"
             type="text" 
           />
 
           <input
+            name="verb"
+            onChange={onInputChange}
+            value={fields.verb}
             placeholder="verb"
             type="text" 
           /> 
           the 
           <input
+            name="adj2"
+            onChange={onInputChange}
+            value={fields.adj2}
             placeholder="adjective"
             type="text" 
           />
 
           <input
-            placeholder="adjective"
+            name="noun2"
+            onChange={onInputChange}
+            value={fields.noun2}
+            placeholder="noun"
             type="noun" 
           />.
 
