cdd382f3ad87631fa18a4e6c5f9f51e2b6f9cd74
thenora <thenora@gmail.com>
2020-04-23 22:36:06 -0700

    notes about refactor needed to recent submission

--- src/components/RecentSubmission.js
+++ src/components/RecentSubmission.js
@@ -4,8 +4,8 @@ import './RecentSubmission.css';
 const RecentSubmission = (props) => {
   return (
     <div className="RecentSubmission">
-      <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{ props.recentSubmission }</p>
+      <h3>The Most Recent Submission</h3> // TODO need to refactor so this doesn't sow
+      <p className="RecentSubmission__submission">{ props.recentSubmission }</p> // TODO should probably generate sentence here, not in Game
     </div>
   );
 }
